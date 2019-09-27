5fd900664aa10315cdec475b1d89a6a42af95b7b
Leon Feng <best.sum@gmail.com>
2019-09-27 11:23:29 -0400

    add react route in the react example

--- examples/react16/App.jsx
+++ examples/react16/App.jsx
@@ -5,18 +5,57 @@
 
 import { Button, Modal, version as antdVersion } from 'antd';
 import React, { useState, version } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default function App() {
+
+function RouteExample() {
+  return (
+    <>
+      <h2>Route Example</h2>
+      <ul>
+        <li>
+          <Link to="/react">React</Link>
+        </li>
+        <li>
+          <Link to="/react/route_example">Route example</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function App() {
 
   const [visible, setVisible] = useState(false);
 
   return (
     <>
       <div style={{ color: 'green' }}>Hello React</div>
+      <div>
+        <div>Try react route</div>
+        <ul>
+          <li>
+            <Link to="/react">React</Link>
+          </li>
+          <li>
+            <Link to="/react/route_example">Route example</Link>
+          </li>
+        </ul>
+      </div>
+
       <Button onClick={() => setVisible(true)}>open antd modal</Button>
       <Modal visible={visible} onOk={() => setVisible(false)} onCancel={() => setVisible(false)}>
         Hello React {version} and antd {antdVersion}
       </Modal>
     </>
+  );
+}
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Route path="/react" exact component={App} />
+      <Route path="/react/route_example" component={RouteExample} />
+    </Router>
   );
 }
