59b0b8b577d70e676ec04e2abc771a95ec1e4d69
Kuitos <kuitos.lau@gmail.com>
2019-11-02 18:54:33 +0800

    :wrench: update eslint and travis config (#125)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     '@typescript-eslint/array-type': ['error', 'array-simple'],
     'no-return-assign': 0,
     'no-console': 0,
-    'max-len': ['error', { code: 120, ignoreComments: true }],
+    'max-len': ['error', { code: 120, ignoreComments: true, ignoreStrings: true }],
     // see https://github.com/prettier/prettier/issues/3847
     'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
   },
