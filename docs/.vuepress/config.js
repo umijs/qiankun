59283389a5bc05c01e9713772ad1e7198c0198f6
Kuitos <kuitos.lau@gmail.com>
2019-11-13 22:33:31 +0800

    :memo:

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'qiankun',
+  description:
+    'An implementation of [Micro Frontends](https://micro-frontends.org/), based on [single-spa](https://github.com/CanopyTax/single-spa), but made it production-ready.',
+};
