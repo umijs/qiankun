2831b1c80b4d476420853bc927c3584ac09498f4
Kuitos <kuitos.lau@gmail.com>
2019-11-29 13:36:29 +0800

    :bug: avoid who using window.window to escape the sandbox environment to touch the really window (#175)

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -64,7 +64,7 @@ export function genSandbox(appName: string, assetPublicPath: string) {
   let inAppSandbox = true;
 
   const boundValueSymbol = Symbol('bound value');
-  const sandbox = new Proxy(window, {
+  const sandbox: WindowProxy = new Proxy(window, {
     set(target: Window, p: PropertyKey, value: any): boolean {
       if (inAppSandbox) {
         if (!target.hasOwnProperty(p)) {
@@ -91,6 +91,12 @@ export function genSandbox(appName: string, assetPublicPath: string) {
     },
 
     get(target: Window, p: PropertyKey) {
+      // avoid who using window.window or window.self to escape the sandbox environment to touch the really window
+      // see https://github.com/eligrey/FileSaver.js/blob/master/src/FileSaver.js#L13
+      if (p === 'window' || p === 'self') {
+        return sandbox;
+      }
+
       const value = (target as any)[p];
       /*
       仅绑定 !isConstructable && isCallable 的函数对象，如 window.console、window.atob 这类。目前没有完美的检测方式，这里通过 prototype 中是否还有可枚举的拓展方法的方式来判断
