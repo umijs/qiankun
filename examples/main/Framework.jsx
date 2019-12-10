a6da5af73e9144b22256592092360c622d04df3b
Kuitos <kuitos.lau@gmail.com>
2019-12-10 12:07:49 +0800

    :pencil: upgrade examples

--- examples/main/Framework.jsx
+++ examples/main/Framework.jsx
@@ -26,9 +26,9 @@ export default function Framework(props) {
         <nav>
           <ol>
             <li><a onClick={() => goto('home', '/')}>home</a></li>
-            <li><a onClick={() => goto('react app', '/react')}>react16 + antd3</a></li>
             <li><a onClick={() => goto('react15 app', '/15react15')}>react15 + antd2</a></li>
             <li><a onClick={() => goto('vue app', '/vue')}>vue2 + element2</a></li>
+            <li><a onClick={() => goto('react app', '/react')}>react16 + antd3</a></li>
           </ol>
         </nav>
       </header>
