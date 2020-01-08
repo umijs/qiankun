8daa3f598a17981b21b47de660c8dd81a3545e01
F-loat <chaimaoyuan@foxmail.com>
2020-01-08 13:45:03 +0800

    :pencil: config vue example bundler

--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { name } = require('./package');
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -41,9 +42,9 @@ module.exports = {
     },
     output: {
       // 把子应用打包成 umd 库格式
-      library: '[name]',
-      filename: '[name].js',
+      library: `${name}-[name]`,
       libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`,
     },
   },
 };
