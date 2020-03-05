eec7d5569dbeca21f6c2ef2204a921ca1ae04d62
Deturium <fanyangliang@gmail.com>
2020-03-06 02:17:17 +0800

    :sparkles: main app vue render

---
+++ examples/main/render/VueRender.js
@@ -0,0 +1,30 @@
+import Vue from 'vue/dist/vue.esm';
+
+function vueRender({ appContent, loading }) {
+  return new Vue({
+    template: `
+      <div id="subapp-container">
+        <h4 v-if="loading" class="subapp-loading">Loading...</h4>
+        <div v-html="appContent" />
+      </div>
+    `,
+    el: '#subapp-container',
+    data() {
+      return {
+        appContent,
+        loading,
+      };
+    },
+  });
+}
+
+let app = null;
+
+export default function render({ appContent, loading }) {
+  if (!app) {
+    app = vueRender({ appContent, loading });
+  } else {
+    app.appContent = appContent;
+    app.loading = loading;
+  }
+}
