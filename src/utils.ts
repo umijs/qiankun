7ce13fe364715694a0d436f2c88ad0e0b25f94d9
fireairforce <1344492820@qq.com>
2020-03-27 23:42:42 +0800

    update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@
 
 import { snakeCase } from 'lodash';
 
-export async function sleep(ms: number) {
+export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
