175629f3e3211c84981a869718a220f4029ff7a4
Kuitos <kuitos.lau@gmail.com>
2020-04-01 12:50:39 +0800

    :green_heart: add unit-test for proxy sandbox (#374)

--- src/sandbox/__tests__/proxySandbox.test.ts
+++ src/sandbox/__tests__/proxySandbox.test.ts
@@ -41,7 +41,7 @@ test('window.top & window.self & window.window should equals with sandbox', () =
   expect(proxy.window).toBe(proxy);
 });
 
-test('hasOwnProperty should work well', () => {
+test('hasOwnProperty should works well', () => {
   const { proxy } = new ProxySandbox('unit-test');
 
   (<any>proxy).testName = 'kuitos';
@@ -50,12 +50,21 @@ test('hasOwnProperty should work well', () => {
 
   expect(proxy.hasOwnProperty('testName')).toBeTruthy();
   expect(window.hasOwnProperty('testName')).toBeFalsy();
+
+  expect(Object.getOwnPropertyDescriptor(proxy, 'testName')).toEqual({
+    value: 'kuitos',
+    configurable: true,
+    enumerable: true,
+    writable: true,
+  });
 });
 
-test('non-configurable property should get correctly from sandbox', () => {
+test('descriptor of non-configurable and non-enumerable property existed in raw window should be the same after modified in sandbox', () => {
   Object.defineProperty(window, 'nonConfigurableProp', { configurable: false, writable: true });
   // eslint-disable-next-line getter-return
   Object.defineProperty(window, 'nonConfigurablePropWithAccessor', { configurable: false, get() {}, set() {} });
+  Object.defineProperty(window, 'enumerableProp', { enumerable: true, writable: true });
+  Object.defineProperty(window, 'nonEnumerableProp', { enumerable: false, writable: true });
 
   const { proxy } = new ProxySandbox('unit-test');
 
@@ -65,5 +74,16 @@ test('non-configurable property should get correctly from sandbox', () => {
   expect(Object.keys(proxy)).toEqual(Object.keys(window));
   expect(Object.getOwnPropertyDescriptor(proxy, 'nonConfigurableProp')).toEqual(
     Object.getOwnPropertyDescriptor(window, 'nonConfigurableProp'),
+  );
+
+  (<any>proxy).enumerableProp = 123;
+  (<any>proxy).nonEnumerableProp = 456;
+  expect((<any>proxy).enumerableProp).toBe(123);
+  expect((<any>proxy).nonEnumerableProp).toBe(456);
+  expect(Object.keys(proxy)).toEqual(Object.keys(window));
+  expect(Object.keys(proxy).includes('nonEnumerableProp')).toBeFalsy();
+  expect(Object.keys(proxy).includes('enumerableProp')).toBeTruthy();
+  expect(Object.getOwnPropertyDescriptor(proxy, 'nonEnumerableProp')).toEqual(
+    Object.getOwnPropertyDescriptor(window, 'nonEnumerableProp'),
   );
 });
