79ef80c8b5f404337780b38ac0f8a4bec8c8bdcc
Kuitos <kuitos.lau@gmail.com>
2020-04-26 17:48:12 +0800

    :sparkles: write System back to global window

--- src/sandbox/proxySandbox.ts
+++ src/sandbox/proxySandbox.ts
@@ -73,6 +73,12 @@ export default class ProxySandbox implements SandBox {
       ]);
     }
 
+    // FIXME see line 98
+    if (this.updateValueMap.has('System')) {
+      // @ts-ignore
+      delete window.System;
+    }
+
     this.sandboxRunning = false;
   }
 
@@ -88,6 +94,14 @@ export default class ProxySandbox implements SandBox {
       set(_: Window, p: PropertyKey, value: any): boolean {
         if (sandboxRunning) {
           updateValueMap.set(p, value);
+
+          // FIXME System.js used a indirect call with eval, which would make it scope escape to global
+          // To make System.js works well, we write it back to global window temporary
+          // see https://github.com/systemjs/systemjs/blob/457f5b7e8af6bd120a279540477552a07d5de086/src/evaluate.js#L106
+          if (p === 'System') {
+            // @ts-ignore
+            rawWindow.System = value;
+          }
 
           return true;
         }
