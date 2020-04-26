f09d981b9d09484f3a32984a44877c13d0500ade
Kuitos <kuitos.lau@gmail.com>
2020-04-26 19:36:54 +0800

    :art:

--- src/sandbox/proxySandbox.ts
+++ src/sandbox/proxySandbox.ts
@@ -5,6 +5,7 @@
 import { uniq } from 'lodash';
 import { SandBox } from '../interfaces';
 import { getProxyPropertyGetter, getTargetValue } from './common';
+import { clearSystemJsProps, interceptSystemJsProps } from './noise/systemjs';
 
 // zone.js will overwrite Object.defineProperty
 const rawObjectDefineProperty = Object.defineProperty;
@@ -73,11 +74,7 @@ export default class ProxySandbox implements SandBox {
       ]);
     }
 
-    // FIXME see line 98
-    if (this.updateValueMap.has('System')) {
-      // @ts-ignore
-      delete window.System;
-    }
+    clearSystemJsProps(this.updateValueMap);
 
     this.sandboxRunning = false;
   }
@@ -95,13 +92,7 @@ export default class ProxySandbox implements SandBox {
         if (sandboxRunning) {
           updateValueMap.set(p, value);
 
-          // FIXME System.js used a indirect call with eval, which would make it scope escape to global
-          // To make System.js works well, we write it back to global window temporary
-          // see https://github.com/systemjs/systemjs/blob/457f5b7e8af6bd120a279540477552a07d5de086/src/evaluate.js#L106
-          if (p === 'System') {
-            // @ts-ignore
-            rawWindow.System = value;
-          }
+          interceptSystemJsProps(p, value);
 
           return true;
         }
