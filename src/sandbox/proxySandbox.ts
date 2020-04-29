1c0767270bde1ab9e7d4057381b45fb12a10eb7c
Kuitos <kuitos.lau@gmail.com>
2020-04-29 21:11:50 +0800

    :checkered_flag: fix the sandbox issue in FF/Safari (#544)

--- src/sandbox/proxySandbox.ts
+++ src/sandbox/proxySandbox.ts
@@ -31,16 +31,24 @@ function createFakeWindow(global: Window): Window {
          see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/get
          > The value reported for a property must be the same as the value of the corresponding target object property if the target object property is a non-writable, non-configurable data property.
          */
-        if (p === 'top' || p === 'self' || p === 'window') {
+        if (
+          p === 'top' ||
+          p === 'self' ||
+          p === 'window' ||
+          (process.env.NODE_ENV === 'test' && (p === 'mockTop' || p === 'mockSafariTop'))
+        ) {
           descriptor.configurable = true;
-          descriptor.writable = true;
+          /*
+           The descriptor of window.window/window.top/window.self in Safari/FF are accessor descriptors, we need to avoid adding a data descriptor while it was
+           Example:
+            Safari/FF: Object.getOwnPropertyDescriptor(window, 'top') -> {get: function, set: undefined, enumerable: true, configurable: false}
+            Chrome: Object.getOwnPropertyDescriptor(window, 'top') -> {value: Window, writable: false, enumerable: true, configurable: false}
+           */
+          if (!Object.prototype.hasOwnProperty.call(descriptor, 'get')) {
+            descriptor.writable = true;
+          }
         }
 
-        // just for test
-        if (process.env.NODE_ENV === 'test' && p === 'mockTop') {
-          descriptor.configurable = true;
-          descriptor.writable = true;
-        }
         // freeze the descriptor to avoid being modified by zone.js
         // see https://github.com/angular/zone.js/blob/a5fe09b0fac27ac5df1fa746042f96f05ccb6a00/lib/browser/define-property.ts#L71
         rawObjectDefineProperty(fakeWindow, p, Object.freeze(descriptor!));
@@ -109,15 +117,15 @@ export default class ProxySandbox implements SandBox {
       },
 
       get(_: Window, p: PropertyKey): any {
-        // just for test
-        if (process.env.NODE_ENV === 'test' && p === 'mockTop') {
-          return proxy;
-        }
-
         // avoid who using window.window or window.self to escape the sandbox environment to touch the really window
         // or use window.top to check if an iframe context
         // see https://github.com/eligrey/FileSaver.js/blob/master/src/FileSaver.js#L13
-        if (p === 'top' || p === 'window' || p === 'self') {
+        if (
+          p === 'top' ||
+          p === 'window' ||
+          p === 'self' ||
+          (process.env.NODE_ENV === 'test' && (p === 'mockTop' || p === 'mockSafariTop'))
+        ) {
           return proxy;
         }
 
