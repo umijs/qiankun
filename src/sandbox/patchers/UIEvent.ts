938f44814326a76c1144d99fa9cdc294e9735a41
howel52 <wuhao.52@outlook.com>
2020-05-14 17:02:36 +0800

    :recycle: FakeMouseEvent extends MouseEvent

--- src/sandbox/patchers/UIEvent.ts
+++ src/sandbox/patchers/UIEvent.ts
@@ -1,7 +1,5 @@
-// @ts-nocheck
-// @todo remove ts-nocheck, add tests
 /**
- * @auther howel52
+ * @author howel52
  * @since 2020-05-13
  */
 
@@ -9,16 +7,15 @@ import { noop } from 'lodash';
 
 const RawMouseEvent = window.MouseEvent;
 
-class FakeMouseEvent {
+class FakeMouseEvent extends MouseEvent {
   constructor(typeArg: string, mouseEventInit?: MouseEventInit) {
-    const { view, ...otherOpts } = mouseEventInit || {};
-
     // if UIEvent want to window view, we should replace ProxyWindow with Window
-    // see: https://github.com/umijs/qiankun/issues/570
-    // eg: https://github.com/apache/incubator-echarts/blob/master/src/component/toolbox/feature/SaveAsImage.js#L63...L75
-    return Object.prototype.toString.call(view) === '[object Window]'
-      ? new RawMouseEvent(typeArg, { view: window, ...otherOpts })
-      : new RawMouseEvent(typeArg, mouseEventInit);
+    if (mouseEventInit && Object.prototype.toString.call(mouseEventInit.view) === '[object Window]') {
+      // resolve: https://github.com/umijs/qiankun/issues/570
+      // eg: https://github.com/apache/incubator-echarts/blob/master/src/component/toolbox/feature/SaveAsImage.js#L63...L75
+      mouseEventInit.view = window;
+    }
+    super(typeArg, mouseEventInit);
   }
 }
 
