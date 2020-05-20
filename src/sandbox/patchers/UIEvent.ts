534d58f1478b27b2dbd88ed64eeb991d0a6f1b55
howel52 <wuhao.52@outlook.com>
2020-05-20 14:11:23 +0800

    :bug: rewrite ProxyWindow in UIEvent (#573)

---
+++ src/sandbox/patchers/UIEvent.ts
@@ -0,0 +1,38 @@
+/**
+ * @author howel52
+ * @since 2020-05-13
+ */
+import { noop } from 'lodash';
+
+const RawMouseEvent = window.MouseEvent;
+
+declare global {
+  interface Window {
+    MouseEvent: MouseEvent | FakeMouseEvent;
+  }
+}
+
+// if ts compile target is es5, the native super/extends has some problems
+// see: https://github.com/microsoft/TypeScript/wiki/FAQ#why-doesnt-extending-built-ins-like-error-array-and-map-work
+class FakeMouseEvent {
+  constructor(typeArg: string, mouseEventInit?: MouseEventInit) {
+    // if UIEvent want to window view, we should replace ProxyWindow with Window
+    if (mouseEventInit && Object.prototype.toString.call(mouseEventInit.view) === '[object Window]') {
+      // resolve: https://github.com/umijs/qiankun/issues/570
+      // eg: https://github.com/apache/incubator-echarts/blob/master/src/component/toolbox/feature/SaveAsImage.js#L63...L75
+      mouseEventInit.view = window;
+    }
+    return new RawMouseEvent(typeArg, mouseEventInit);
+  }
+}
+// set prototype
+Object.setPrototypeOf(FakeMouseEvent, RawMouseEvent.prototype);
+
+
+export default function patch(global: Window) {
+  global.MouseEvent = FakeMouseEvent;
+  return function free() {
+    global.MouseEvent = RawMouseEvent;
+    return noop;
+  };
+}
---
+++ src/sandbox/patchers/__tests__/UIEvent.test.ts
@@ -0,0 +1,38 @@
+/**
+ * @author howel52
+ * @since 2020-05-14
+ */
+
+import patch from '../UIEvent';
+
+test('patch UIEvent', async () => {
+  const free = patch(window);
+
+  const dispatchEventAction = jest.fn();
+
+  const $a = document.createElement('a');
+  $a.onclick = dispatchEventAction;
+  const evt = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: false,
+  });
+  $a.dispatchEvent(evt);
+
+  expect(dispatchEventAction).toBeCalledTimes(1);
+
+  // free
+  free();
+
+  // @todo fixme, 使用 proxySandbox 代替 window
+  // 由于 jsdom UIEvent 目前对 ProxyWindow 并不会 throw TypeError, 与浏览器行为不一致
+  // see: https://github.com/jsdom/jsdom/issues/2973
+  // expect(() => {
+  //   const evt2 = new MouseEvent('click', {
+  //     view: new Proxy(window, {}),
+  //     bubbles: true,
+  //     cancelable: false
+  //   });
+  //   $a.dispatchEvent(evt2);
+  // }).toThrow("Failed to construct 'MouseEvent': member view is not of type Window.");
+});
