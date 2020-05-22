cdaddb2e3f236ab9991a0a8035d19a79a7a5a2ca
howel52 <wuhao.52@outlook.com>
2020-05-22 10:50:38 +0800

    :white_check_mark: ts compile to es6 & add MouseEvent error case (#593)

--- src/sandbox/patchers/UIEvent.ts
+++ src/sandbox/patchers/UIEvent.ts
@@ -2,19 +2,19 @@
  * @author howel52
  * @since 2020-05-13
  */
+
 import { noop } from 'lodash';
 
 const RawMouseEvent = window.MouseEvent;
 
 declare global {
   interface Window {
-    MouseEvent: MouseEvent | FakeMouseEvent;
+    MouseEvent: MouseEvent;
   }
 }
-
 // if ts compile target is es5, the native super/extends has some problems
 // see: https://github.com/microsoft/TypeScript/wiki/FAQ#why-doesnt-extending-built-ins-like-error-array-and-map-work
-class FakeMouseEvent {
+class FakeMouseEvent extends RawMouseEvent {
   constructor(typeArg: string, mouseEventInit?: MouseEventInit) {
     // if UIEvent want to window view, we should replace ProxyWindow with Window
     if (mouseEventInit && Object.prototype.toString.call(mouseEventInit.view) === '[object Window]') {
@@ -22,11 +22,9 @@ class FakeMouseEvent {
       // eg: https://github.com/apache/incubator-echarts/blob/master/src/component/toolbox/feature/SaveAsImage.js#L63...L75
       mouseEventInit.view = window;
     }
-    return new RawMouseEvent(typeArg, mouseEventInit);
+    super(typeArg, mouseEventInit);
   }
 }
-// set prototype
-Object.setPrototypeOf(FakeMouseEvent, RawMouseEvent);
 
 export default function patch(global: Window) {
   global.MouseEvent = FakeMouseEvent;
