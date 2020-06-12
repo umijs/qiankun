72bf3da0ab8d3bff71c09c2d83b0142491e954d3
Kuitos <kuitos.lau@gmail.com>
2020-06-12 12:47:22 +0800

    :rotating_light: remove lint warning (#656)

--- src/addons/engineFlag.ts
+++ src/addons/engineFlag.ts
@@ -8,14 +8,17 @@ import { FrameworkLifeCycles } from '../interfaces';
 export default function getAddOn(global: Window): FrameworkLifeCycles<any> {
   return {
     async beforeLoad() {
+      // eslint-disable-next-line no-param-reassign
       global.__POWERED_BY_QIANKUN__ = true;
     },
 
     async beforeMount() {
+      // eslint-disable-next-line no-param-reassign
       global.__POWERED_BY_QIANKUN__ = true;
     },
 
     async beforeUnmount() {
+      // eslint-disable-next-line no-param-reassign
       delete global.__POWERED_BY_QIANKUN__;
     },
   };
