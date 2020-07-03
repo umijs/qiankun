b57762d2c9e898e7cc4f3a5e17a658785520b4b2
howel52 <wuhao.52@outlook.com>
2020-07-03 10:52:30 +0800

    :sparkles: patch on demand (#709)

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -84,9 +84,20 @@ export type Rebuilder = () => void;
 export type Freer = () => Rebuilder;
 export type Patcher = () => Freer;
 
+export enum SandBoxType {
+  Proxy = 'Proxy',
+  Snapshot = 'Snapshot',
+
+  // for legacy sandbox
+  // https://github.com/umijs/qiankun/blob/0d1d3f0c5ed1642f01854f96c3fabf0a2148bd26/src/sandbox/legacy/sandbox.ts#L22...L25
+  LegacyProxy = 'LegacyProxy',
+}
+
 export interface SandBox {
   /** 沙箱的名字 */
   name: string;
+  /** 沙箱的类型 */
+  type: SandBoxType;
   /** 沙箱导出的代理实体 */
   proxy: WindowProxy;
   /** 沙箱是否在运行中 */
--- src/sandbox/patchers/index.ts
+++ src/sandbox/patchers/index.ts
@@ -3,7 +3,7 @@
  * @since 2019-04-11
  */
 
-import { Freer } from '../../interfaces';
+import { Freer, SandBox, SandBoxType } from '../../interfaces';
 import patchDynamicAppend from './dynamicAppend';
 import patchHistoryListener from './historyListener';
 import patchInterval from './interval';
@@ -13,23 +13,38 @@ import patchUIEvent from './UIEvent';
 export function patchAtMounting(
   appName: string,
   elementGetter: () => HTMLElement | ShadowRoot,
-  proxy: Window,
+  sandbox: SandBox,
   singular: boolean,
 ): Freer[] {
-  return [
-    patchInterval(),
-    patchWindowListener(),
-    patchHistoryListener(),
-    patchDynamicAppend(appName, elementGetter, proxy, true, singular),
-    patchUIEvent(proxy),
+  const basePatchers = [
+    () => patchInterval(),
+    () => patchWindowListener(),
+    () => patchHistoryListener(),
+    () => patchDynamicAppend(appName, elementGetter, sandbox.proxy, true, singular),
   ];
+
+  const patchersInSandbox = {
+    [SandBoxType.LegacyProxy]: [...basePatchers, () => patchUIEvent(sandbox.proxy)],
+    [SandBoxType.Proxy]: [...basePatchers, () => patchUIEvent(sandbox.proxy)],
+    [SandBoxType.Snapshot]: basePatchers,
+  };
+
+  return patchersInSandbox[sandbox.type]?.map(patch => patch());
 }
 
 export function patchAtBootstrapping(
   appName: string,
   elementGetter: () => HTMLElement | ShadowRoot,
-  proxy: Window,
+  sandbox: SandBox,
   singular: boolean,
 ): Freer[] {
-  return [patchDynamicAppend(appName, elementGetter, proxy, false, singular)];
+  const basePatchers = [() => patchDynamicAppend(appName, elementGetter, sandbox.proxy, false, singular)];
+
+  const patchersInSandbox = {
+    [SandBoxType.LegacyProxy]: basePatchers,
+    [SandBoxType.Proxy]: basePatchers,
+    [SandBoxType.Snapshot]: basePatchers,
+  };
+
+  return patchersInSandbox[sandbox.type]?.map(patch => patch());
 }
