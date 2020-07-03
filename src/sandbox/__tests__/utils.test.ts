60537fa8de767b1a46caf8c1cb5f30813bc82b3e
Kuitos <kuitos.lau@gmail.com>
2020-07-03 17:34:50 +0800

    :green_heart: fix unit test

--- src/sandbox/__tests__/utils.test.ts
+++ src/sandbox/__tests__/utils.test.ts
@@ -54,7 +54,7 @@ test('should be able to suspend', async () => {
   const end = new Date().getTime();
 
   const diff = end - start;
-  expect(diff >= 10 && diff < 100).toBeTruthy();
+  expect(diff >= 10 && diff < 1000).toBeTruthy();
 });
 
 test('Deferred should worked [1]', async () => {
