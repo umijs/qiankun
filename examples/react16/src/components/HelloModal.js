5a89e244e01e16a0b67b02b0008873e369544868
Kuitos <kuitos.lau@gmail.com>
2020-08-25 18:11:00 +0800

    🐛 fix master example

--- examples/react16/src/components/HelloModal.js
+++ examples/react16/src/components/HelloModal.js
@@ -1,24 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Modal } from 'antd';
 
-const dispatchUIEvent = () => {
-  const $a = document.createElement('a');
-  $a.onclick = () => {
-    console.log('log from UIEvent');
-  };
-  const evt = new MouseEvent('click', {
-    view: window,
-    bubbles: true,
-    cancelable: false,
-  });
-  $a.dispatchEvent(evt);
-};
-
 export default function() {
   const [visible, setVisible] = useState(false);
-  useEffect(() => {
-    dispatchUIEvent();
-  }, []);
   return (
     <>
       <Button onClick={() => setVisible(true)}>CLICK ME</Button>
