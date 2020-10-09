8f8593b3f277585fb5a679eeefd19393b1baef75
chaunjie <chaunjie@dingtalk.com>
2020-10-09 17:23:14 +0800

    #0: add vue3 demo

---
+++ examples/vue3/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-content">
+    <h1>This is about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  setup() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.about-content {
+  color: #7265e6;
+}
+</style>
---
+++ examples/vue3/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <HelloWorld msg="Vue.js Demo" @close="close"> </HelloWorld>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  created() {
+    console.log('vue2.0写法 created');
+  },
+  mounted() {
+    console.log(this.$route);
+  },
+  methods: {
+    close() {
+      console.log('close');
+    },
+  },
+};
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
