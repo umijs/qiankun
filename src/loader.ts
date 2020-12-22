ba4308ab8a1ee246d4ac01aeaf04eaf8d861f4e2
李海涛 <liht@dustess.com>
2020-12-22 16:49:57 +0800

    fix not suppot performance.getEntriesByName

--- src/loader.ts
+++ src/loader.ts
@@ -355,7 +355,7 @@ export async function loadApp<T extends object>(
       bootstrap,
       mount: [
         async () => {
-          if (process.env.NODE_ENV === 'development') {
+          if (process.env.NODE_ENV === 'development' && performance.getEntriesByName) {
             const marks = performance.getEntriesByName(markName, 'mark');
             // mark length is zero means the app is remounting
             if (!marks.length) {
--- src/utils.ts
+++ src/utils.ts
@@ -101,7 +101,8 @@ const supportsUserTiming =
   typeof performance.mark === 'function' &&
   typeof performance.clearMarks === 'function' &&
   typeof performance.measure === 'function' &&
-  typeof performance.clearMeasures === 'function';
+  typeof performance.clearMeasures === 'function' &&
+  typeof performance.getEntriesByName === 'function';
 
 export function performanceMark(markName: string) {
   if (supportsUserTiming) {
