ee90b6d771e08d56e4e5564f85aa22c7bf2117ab
ice-zjchen <ice.zjchen@qq.com>
2021-04-12 11:38:30 +0800

    build: change example packageName

--- examples/main/webpack.config.js
+++ examples/main/webpack.config.js
@@ -31,7 +31,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  modules: 'commonjs',
+                }
+              ]
+            ],
             plugins: ['@babel/plugin-transform-react-jsx'],
           },
         },
