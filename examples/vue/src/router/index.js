e088956bef1192312e846c410730208d4e41ebaa
406087475 <406087475@qq.com>
2021-11-04 18:10:53 +0800

    bugfixed vue2 demo  cannot run
    bugfixed vue2 demo  cannot run

--- examples/vue/src/router/index.js
+++ examples/vue/src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import App from '../App.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+const routes = [{
+  path: '/vue',
+  component: App,
+  children: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: 'about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+  ]
+}]
 
 export default routes;
