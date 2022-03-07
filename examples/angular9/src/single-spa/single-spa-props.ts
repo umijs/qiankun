393e504d4e48217857f29b904f9e042932541008
ice-zjchen <ice.zjchen@gmail.com>
2022-03-07 11:55:51 +0800

    rm examples

--- examples/angular9/src/single-spa/single-spa-props.ts
+++
@@ -1,8 +0,0 @@
-import { ReplaySubject } from 'rxjs';
-import { AppProps } from 'single-spa';
-
-export const singleSpaPropsSubject = new ReplaySubject<SingleSpaProps>(1);
-
-// Add any custom single-spa props you have to this type def
-// https://single-spa.js.org/docs/building-applications.html#custom-props
-export type SingleSpaProps = AppProps & {};
