526a3f610c4f5f2794e138d390522f7bcc30a25f
Kuitos <kuitos.lau@gmail.com>
2022-05-23 12:18:22 +0800

    🐛 simulate micro app head element for dynamic appending styles (#2097)

--- src/addons/index.ts
+++ src/addons/index.ts
@@ -5,9 +5,8 @@
 
 import { concat, mergeWith } from 'lodash';
 import type { FrameworkLifeCycles, ObjectType } from '../interfaces';
-
-import getRuntimePublicPathAddOn from './runtimePublicPath';
 import getEngineFlagAddon from './engineFlag';
+import getRuntimePublicPathAddOn from './runtimePublicPath';
 
 export default function getAddOns<T extends ObjectType>(global: Window, publicPath: string): FrameworkLifeCycles<T> {
   return mergeWith({}, getEngineFlagAddon(global), getRuntimePublicPathAddOn(global, publicPath), (v1, v2) =>
