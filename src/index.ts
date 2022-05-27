d99300214a4810820e1ad5b983bfd262aaebbe34
liaohainan <liaohainan44@126.com>
2022-05-27 21:28:47 +0800

    feat:add unregisterMicroApp method

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@
  * @since 2019-04-25
  */
 
-export { loadMicroApp, registerMicroApps, start } from './apis';
-export { initGlobalState } from './globalState';
-export { getCurrentRunningApp as __internalGetCurrentRunningApp } from './sandbox';
-export * from './errorHandler';
-export * from './effects';
-export * from './interfaces';
-export { prefetchImmediately as prefetchApps } from './prefetch';
+ export { loadMicroApp, registerMicroApps, start, unregisterMicroApp } from './apis';
+ export { initGlobalState } from './globalState';
+ export { getCurrentRunningApp as __internalGetCurrentRunningApp } from './sandbox';
+ export * from './errorHandler';
+ export * from './effects';
+ export * from './interfaces';
+ export { prefetchImmediately as prefetchApps } from './prefetch';
+ 
