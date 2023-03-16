0c7d28a8e79e8b2aabc3e96a77d951db1e88fa41
liuweihua <liuweihua@stary.com>
2023-03-16 21:11:35 +0800

    feat: add __zone_symbol__setTimeout type in global field

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -12,6 +12,7 @@ declare global {
     __INJECTED_PUBLIC_PATH_BY_QIANKUN__?: string;
     __QIANKUN_DEVELOPMENT__?: boolean;
     Zone?: CallableFunction;
+    __zone_symbol__setTimeout?: Window['setTimeout'];
   }
 }
 
