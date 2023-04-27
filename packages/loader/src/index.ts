54e4cd33d78d8b2e11d447b366765c35ad231a9a
Kuitos <kuitos.lau@gmail.com>
2023-04-27 20:28:44 +0800

    feat: support multiple apps

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -31,7 +31,8 @@ export async function loadEntry(entry: Entry, container: HTMLElement, opts?: Imp
 
   const res = await fetch(entry);
   if (res.body) {
-    const deferred = new Deferred<void>();
+    const loadEntryDeferred = new Deferred<void>();
+    const streamFinishedDeferred = new Deferred<void>();
 
     res.body
       .pipeThrough(new TextDecoderStream())
@@ -40,23 +41,37 @@ export async function loadEntry(entry: Entry, container: HTMLElement, opts?: Imp
           const transformedNode = nodeTransformer(node, entry, { fetch, compartment });
 
           const script = transformedNode as any as HTMLScriptElement;
-          if (script.tagName === 'SCRIPT' && script.hasAttribute('entry')) {
-            script.addEventListener('load', () => deferred.resolve(), { once: true });
+          if (script.tagName === 'SCRIPT' && (script.src || script.dataset.src)) {
+            script.addEventListener(
+              'load',
+              async () => {
+                /*
+                 * If the entry script is executed, we can complete the entry process in advance
+                 * otherwise we need to wait until the last script is executed.
+                 */
+                if (script.hasAttribute('entry')) {
+                  loadEntryDeferred.resolve();
+                } else {
+                  await streamFinishedDeferred.promise;
+
+                  const scripts = container.querySelectorAll('script[src]');
+                  const lastScript = scripts[scripts.length - 1];
+
+                  if (lastScript === script) {
+                    loadEntryDeferred.resolve();
+                  }
+                }
+              },
+              { once: true },
+            );
           }
 
           return transformedNode;
         }),
       )
-      .then(() => {
-        const scripts = container.querySelector('script[src]');
-        if (scripts && scripts.lastChild) {
-          scripts.lastChild.addEventListener('load', () => deferred.resolve(), { once: true });
-        } else {
-          deferred.resolve();
-        }
-      });
+      .then(() => streamFinishedDeferred.resolve());
 
-    await deferred.promise;
+    return loadEntryDeferred.promise;
   }
 
   // return {} as any;
