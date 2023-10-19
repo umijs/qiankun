a1f2c8151b3e6656553f5b29c32e716c3cca7cfc
Hermanna <896559147@qq.com>
2023-10-19 16:12:01 +0800

    feat(tests): add automated test cases for QiankunPlugin

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,58 +1,90 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import fs from 'fs';
 import path from 'path';
 import type { Compiler, Configuration } from 'webpack';
 import { RawSource } from 'webpack-sources';
 
-const projectRoot: string = process.cwd();
-const packageJsonPath: string = path.join(projectRoot, 'package.json');
-const packageJson: { name?: string } = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-
 interface QiankunPluginOptions {
   packageName?: string;
 }
 
+interface PackageJson {
+  name?: string;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+}
+
 class QiankunPlugin {
   private packageName: string;
+  private static packageJson: PackageJson = QiankunPlugin.readPackageJson();
 
   constructor(options: QiankunPluginOptions = {}) {
-    this.packageName = options.packageName || packageJson.name || '';
+    this.packageName = options.packageName || QiankunPlugin.packageJson.name || '';
+  }
+
+  private static readPackageJson(): { name?: string } {
+    const projectRoot: string = process.cwd();
+    const packageJsonPath: string = path.join(projectRoot, 'package.json');
+    return JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+  }
+
+  private static getWebpackVersion(): string {
+    return QiankunPlugin.packageJson.dependencies?.webpack || QiankunPlugin.packageJson.devDependencies?.webpack || '';
   }
 
   apply(compiler: Compiler): void {
-    const webpackCompilerOptions = compiler.options as Configuration & { output: { jsonpFunction?: string } };
-    webpackCompilerOptions.output.library = `${this.packageName}`;
-    webpackCompilerOptions.output.libraryTarget = 'window';
-    webpackCompilerOptions.output.jsonpFunction = `webpackJsonp_${this.packageName}`;
-    webpackCompilerOptions.output.globalObject = 'window';
-    webpackCompilerOptions.output.chunkLoadingGlobal = `webpackJsonp_${this.packageName}`;
-
+    this.configureWebpackOutput(compiler);
     compiler.hooks.emit.tapAsync('QiankunPlugin', (compilation, callback) => {
-      Object.keys(compilation.assets).forEach((filename) => {
-        if (filename.endsWith('.html')) {
-          const htmlSource = compilation.assets[filename].source();
-          const htmlString = typeof htmlSource === 'string' ? htmlSource : htmlSource.toString('utf-8');
-
-          // 找到所有的 <script> 标签
-          const scriptTags = htmlString.match(/<script[^>]*src="[^"]+"[^>]*><\/script>/g) || [];
-          // 筛选出不包含 defer 和 async 属性的标签
-          const nonAsyncOrDeferScripts = scriptTags.filter((tag) => !/defer|async/.test(tag));
-
-          if (nonAsyncOrDeferScripts.length) {
-            // 获取最后一个 <script> 标签
-            const lastScriptTag = nonAsyncOrDeferScripts[nonAsyncOrDeferScripts.length - 1];
-            // 添加 entry 属性
-            const modifiedScriptTag = lastScriptTag.replace('<script', '<script entry');
-            const modifiedHtml = htmlString.replace(lastScriptTag, modifiedScriptTag);
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            compilation.assets[filename] = new RawSource(modifiedHtml) as any;
-          }
-        }
-      });
+      this.modifyHtmlAssets(compilation);
       callback();
     });
   }
+
+  private configureWebpackOutput(compiler: Compiler): void {
+    const webpackVersion = QiankunPlugin.getWebpackVersion();
+    const webpackCompilerOptions = compiler.options as Configuration & { output: { jsonpFunction?: string } };
+    if (webpackVersion.startsWith('4')) {
+      // webpack 4
+      webpackCompilerOptions.output.library = `${this.packageName}`;
+      webpackCompilerOptions.output.libraryTarget = 'window';
+      webpackCompilerOptions.output.jsonpFunction = `webpackJsonp_${this.packageName}`;
+      webpackCompilerOptions.output.globalObject = 'window';
+      webpackCompilerOptions.output.chunkLoadingGlobal = `webpackJsonp_${this.packageName}`;
+    } else if (webpackVersion.startsWith('5')) {
+      // webpack 5
+      webpackCompilerOptions.output.library = {
+        name: `${this.packageName}`,
+        type: 'window',
+      };
+      webpackCompilerOptions.output.libraryTarget = 'window';
+      webpackCompilerOptions.output.jsonpFunction = `webpackJsonp_${this.packageName}`;
+      webpackCompilerOptions.output.globalObject = 'window';
+      webpackCompilerOptions.output.chunkLoadingGlobal = `webpackJsonp_${this.packageName}`;
+    }
+  }
+
+  private modifyHtmlAssets(compilation: any): void {
+    Object.keys(compilation.assets).forEach((filename) => {
+      if (filename.endsWith('.html')) {
+        const htmlSource = compilation.assets[filename].source();
+        const htmlString = typeof htmlSource === 'string' ? htmlSource : htmlSource.toString('utf-8');
+
+        const modifiedHtml = this.addEntryAttributeToScripts(htmlString);
+        compilation.assets[filename] = new RawSource(modifiedHtml);
+      }
+    });
+  }
+
+  private addEntryAttributeToScripts(htmlString: string): string {
+    const scriptTags = htmlString.match(/<script[^>]*src="[^"]+"[^>]*><\/script>/g) || [];
+    const nonAsyncOrDeferScripts = scriptTags.filter((tag) => !/defer|async/.test(tag));
+
+    if (nonAsyncOrDeferScripts.length) {
+      const lastScriptTag = nonAsyncOrDeferScripts[nonAsyncOrDeferScripts.length - 1];
+      const modifiedScriptTag = lastScriptTag.replace('<script', '<script entry');
+      return htmlString.replace(lastScriptTag, modifiedScriptTag);
+    }
+    return htmlString;
+  }
 }
 
-// 使用export default导出，引用时需要增加.default，不太友好，所以这里使用cjs导出。
 module.exports = QiankunPlugin;
---
+++ packages/webpack-plugin/tests/webpack4/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const QiankunPlugin = require('../../dist/cjs'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html', // 指定模板文件路径
+        filename: 'index.html'   // 输出的HTML文件名（默认为index.html）
+      }),
+    new QiankunPlugin(),
+]
+};
