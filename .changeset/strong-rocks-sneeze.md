bd12dbadae78cc201e1c5fb30eaf9c641638ceb8
Kuitos <kuitos.lau@gmail.com>
2023-11-17 19:00:46 +0800

    fix: defer scripts should wait until html loaded (#2816)

---
+++ .changeset/strong-rocks-sneeze.md
@@ -0,0 +1,7 @@
+---
+"@qiankunjs/loader": patch
+"@qiankunjs/sandbox": patch
+"@qiankunjs/shared": patch
+---
+
+fix: defer scripts should wait until html loaded
--- packages/sandbox/src/patchers/dynamicAppend/types.ts
+++ packages/sandbox/src/patchers/dynamicAppend/types.ts
@@ -3,12 +3,13 @@
  * @since 2023-05-04
  */
 import type { BaseLoaderOpts, NodeTransformer } from '@qiankunjs/shared';
+import type { Deferred } from '@qiankunjs/shared';
 import type { Sandbox } from '../../core/sandbox';
 
 export type SandboxConfig = {
   appName: string;
   sandbox: Sandbox;
   dynamicStyleSheetElements: Array<HTMLStyleElement | HTMLLinkElement>;
-  dynamicExternalSyncScriptElements: HTMLScriptElement[];
+  dynamicExternalSyncScriptDeferredList: Array<Deferred<void>>;
   nodeTransformer: NodeTransformer;
 } & BaseLoaderOpts;
