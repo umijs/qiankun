f5dfad72fdb49025310a550fa997481f3f78b6e9
qiYuei <www.897359641@qq.com>
2023-11-18 15:08:33 +0800

    chore: child app increase scoped

--- packages/create-qiankun/template/vue2-webpack-sub/src/App.vue
+++ packages/create-qiankun/template/vue2-webpack-sub/src/App.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- packages/create-qiankun/template/vue3-webpack-sub/src/App.vue
+++ packages/create-qiankun/template/vue3-webpack-sub/src/App.vue
@@ -7,7 +7,7 @@
   <router-view />
 </template>
 
-<style lang="less">
+<style lang="less" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
