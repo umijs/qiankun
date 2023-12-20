975bb6ddcfd46d24259849f9c8ebfaabea7bbce6
wanghangit <724690450@qq.com>
2023-12-20 22:13:50 +0800

    fix: add mockSandbox at unit test

--- packages/shared/src/assets-transpilers/__tests__/script.test.ts
+++ packages/shared/src/assets-transpilers/__tests__/script.test.ts
@@ -1,12 +1,28 @@
 import { expect, it } from 'vitest';
 import transpileScript from '../script';
-import { StandardSandbox } from '../../../../sandbox/src/core/sandbox/StandardSandbox';
 
 it('inline script not include sourceURL', () => {
+  class MockSandbox {
+    id = 'testApp';
+    constantIntrinsicNames = [];
+    makeEvaluateFactory(source: string, sourceURL?: string): string {
+      const sourceMapURL = sourceURL ? `//# sourceURL=${sourceURL}\n` : '';
+      const globalObjectOptimizer = this.constantIntrinsicNames.length
+        ? `const {${this.constantIntrinsicNames.join(',')}} = this;`
+        : '';
+      // eslint-disable-next-line max-len
+      return `;(function(){with(this){${globalObjectOptimizer}${source}\n${sourceMapURL}}}).bind(window.${this.id})();`;
+    }
+  }
+
   const scriptElement = document.createElement('script');
   scriptElement.innerHTML = 'console.log("hello world")';
-  const sandboxInstance = new StandardSandbox("app", {});
+  const sandboxInstance = new MockSandbox();
   const publicPath = 'http://localhost:8000';
-  const transpiledScriptElement = transpileScript(scriptElement, publicPath, {fetch: window.fetch, rawNode: scriptElement, sandbox: sandboxInstance});
+  const transpiledScriptElement = transpileScript(scriptElement, publicPath, {
+    fetch: window.fetch,
+    rawNode: scriptElement,
+    sandbox: sandboxInstance,
+  });
   expect(transpiledScriptElement.innerHTML).toEqual(expect.not.stringContaining(`//# sourceURL=${publicPath}`));
 });
