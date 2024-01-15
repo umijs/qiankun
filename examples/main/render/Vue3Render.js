6fa797dce97123726d5fdb45f763f0e2f3312864
Linghao Su <slh001@live.cn>
2024-01-15 23:03:29 +0800

    fix(vue): fix mount app not await unmount

--- examples/main/render/Vue3Render.js
+++ examples/main/render/Vue3Render.js
@@ -1,6 +1,12 @@
-import { createApp, h } from 'vue';
+import { createApp, h, onBeforeMount, onBeforeUnmount, ref } from 'vue';
 import { MicroApp } from '../../../packages/ui-bindings/vue/dist/esm';
 
+const sidemenu = document.querySelector('.mainapp-sidemenu');
+
+const microApps = [
+  { name: 'react15', entry: '//localhost:7102' },
+  { name: 'react16', entry: '//localhost:7100' },
+];
 
 function vueRender() {
   const application = createApp({
@@ -8,16 +14,35 @@ function vueRender() {
     },
     render() {
       return h('div', [
-        this.message,
-        h(MicroApp, { name: 'react15', entry: '//localhost:7102' }),
-        this.message,
+        h(MicroApp, { name: this.name, entry: this.entry, autoCaptureError: true }),
       ]);
     },
     setup() {
-      const message = 'abc';
+      const name = ref('');
+      const entry = ref('');
+
+      const handleMenuClick = (e) => {
+        const app = microApps.find((app) => app.name === e.target.dataset.value);
+        if (app && app.name !== name.value) {
+          name.value = app.name;
+          entry.value = app.entry;
+        } else {
+          console.log('not found any app');
+        }
+      }
+
+      onBeforeMount(() => {
+        sidemenu.addEventListener('click', handleMenuClick);
+      });
+
+
+      onBeforeUnmount(() => {
+        sidemenu.removeEventListener('click', handleMenuClick);
+      });
 
       return {
-        message,
+        name,
+        entry,
       };
       // return () => h('div', [
       //   message.value,
--- packages/qiankun/src/version.ts
+++ packages/qiankun/src/version.ts
@@ -1,1 +1,1 @@
-export { version } from '../package.json';
+export const version = '3.0.0-rc.19';
