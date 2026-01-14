28b0d022444ccccc08200e1b1f66d50805ecfc4b
kuitos <kuitos.lau@gmail.com>
2026-01-15 00:15:19 +0800

    fix(create-qiankun): restore eslint-disable for test file

--- packages/create-qiankun/tests/e2e.cli.test.ts
+++ packages/create-qiankun/tests/e2e.cli.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
+
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import execa from 'execa';
 import fse from 'fs-extra';
